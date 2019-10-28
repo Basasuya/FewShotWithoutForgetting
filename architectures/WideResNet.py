9daaaadc4aff2d8e2a748bda3b769e690a4309a8
Basasuya <961256668@qq.com>
2019-10-28 14:25:50 +0800

    add WRN net

---
+++ architectures/WideResNet.py
@@ -0,0 +1,106 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResBlock(nn.Module):
+    def __init__(self, nFin, nFout, stride=1, dropout_rate=0):
+        super(ResBlock, self).__init__()
+        self.equalInOut = (nFin == nFout and stride == 1)
+
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(nFin, nFout, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(nFout),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(nFout, nFout, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(nFout),
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or nFin != nFout:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(nFin, nFout, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(nFout),
+            )
+
+    def forward(self, x):
+        identity = x
+        out = F.relu(self.conv_block(x) + self.shortcut(identity))
+        return out
+
+class WideResNet(nn.Module):
+    def __init__(self, opt):
+        depth = opt['depth']
+        widen_factor = opt['widen_factor']
+        self.use_relu = opt['userelu']
+
+        super(WideResNet, self).__init__()
+        self.in_planes = 16
+
+        assert ((depth-4)%6 ==0), 'Wide-resnet depth should be 6n+4'
+        n = int((depth-4)/6)
+        k = widen_factor
+
+
+        nStages = [16, 16*k, 32*k, 64*k]
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, nStages[0], kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(nStages[0]),
+            nn.ReLU(inplace=True)
+        )
+
+
+        self.layer1 = self._wide_layer(ResBlock, nStages[1], n, stride=2)
+        self.layer2 = self._wide_layer(ResBlock, nStages[2], n, stride=2)
+        self.layer3 = self._wide_layer(ResBlock, nStages[3], n, stride=2)
+
+        # self.bn = nn.BatchNorm2d(nStages[3], momentum=0.9)
+        # self.bn = nn.BatchNorm2d(nStages[3])
+
+        # if 'SS' in opt:
+        #     self.SS = True
+        #     self.layer_SS = self._wide_layer(ResBlock, nStages[3], n, stride=2)
+        #     # self.bn_SS = nn.BatchNorm2d(nStages[3], momentum=0.9)
+        #     self.bn_SS = nn.BatchNorm2d(nStages[3])
+        # else:
+        #     self.SS = False
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _wide_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes
+        # layers.append(nn.BatchNorm2d(planes))
+        # layers.append(nn.ReLU(inplace=True))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # import pdb; pdb.set_trace()
+        out = self.conv1(x)
+        # import pdb; pdb.set_trace()
+        out = self.layer1(out)
+        # import pdb; pdb.set_trace()
+        out = self.layer2(out)
+        out = self.layer3(out)
+
+        # import pdb; pdb.set_trace()
+        out = F.avg_pool2d(out, 10)
+        out = out.view(out.size(0), -1)
+        # import pdb; pdb.set_trace()
+
+        return out, out
+
+def create_model(opt):
+    return WideResNet(opt)
--- config/miniImageNet_WRNLikeCosineClassifier.py
+++ config/miniImageNet_WRNLikeCosineClassifier.py
@@ -27,12 +27,12 @@ config['data_test_opt'] = data_test_opt
 config['max_num_epochs'] = 60
 
 networks = {}
-net_optionsF = {'userelu': False, 'in_planes':3, 'out_planes':[64,64,128,128], 'num_stages':4}
+net_optionsF = {'userelu': False, 'depth': 28, 'widen_factor' : 10}
 net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.1),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
-networks['feat_model'] = {'def_file': 'architectures/ConvNet.py', 'pretrained': None, 'opt': net_optionsF, 'optim_params': net_optim_paramsF}
+networks['feat_model'] = {'def_file': 'architectures/WideResNet.py', 'pretrained': None, 'opt': net_optionsF, 'optim_params': net_optim_paramsF}
 
 net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.1),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
-net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'none', 'nKall': nKbase, 'nFeat':128*5*5, 'scale_cls': 10}
+net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'none', 'nKall': nKbase, 'nFeat': 6400, 'scale_cls': 10}
 networks['classifier'] = {'def_file': 'architectures/ClassifierWithFewShotGenerationModule.py', 'pretrained': None, 'opt': net_optionsC, 'optim_params': net_optim_paramsC}
 
 config['networks'] = networks
