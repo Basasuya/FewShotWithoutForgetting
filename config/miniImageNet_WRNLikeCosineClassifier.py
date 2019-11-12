eecc70e416ed82999124ddfca1b145f6dbcd74a6
Basasuya <961256668@qq.com>
2019-11-12 11:06:19 +0800

    modify WRN

--- config/miniImageNet_WRNLikeCosineClassifier.py
+++ config/miniImageNet_WRNLikeCosineClassifier.py
@@ -28,10 +28,10 @@ config['max_num_epochs'] = 60
 
 networks = {}
 net_optionsF = {'userelu': False, 'depth': 28, 'widen_factor' : 10}
-net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.1),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
+net_optim_paramsF = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.1),(40, 0.01),(60, 0.001)]}
 networks['feat_model'] = {'def_file': 'architectures/WideResNet.py', 'pretrained': None, 'opt': net_optionsF, 'optim_params': net_optim_paramsF}
 
-net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.1),(40, 0.006),(50, 0.0012),(60, 0.00024)]}
+net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.1, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(20, 0.1),(40, 0.01),(60, 0.001)]}
 net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'none', 'nKall': nKbase, 'nFeat': 640, 'scale_cls': 10}
 networks['classifier'] = {'def_file': 'architectures/ClassifierWithFewShotGenerationModule.py', 'pretrained': None, 'opt': net_optionsC, 'optim_params': net_optim_paramsC}
 
--- config/miniImageNet_WRNLikeCosineClassifierAttN1.py
+++ config/miniImageNet_WRNLikeCosineClassifierAttN1.py
@@ -11,7 +11,7 @@ data_train_opt['nKbase'] = -1
 data_train_opt['nExemplars'] = nExemplars
 data_train_opt['nTestNovel'] = nKnovel * 3
 data_train_opt['nTestBase'] = nKnovel * 3 
-data_train_opt['batch_size'] = 4
+data_train_opt['batch_size'] = 1
 data_train_opt['epoch_size'] = data_train_opt['batch_size'] * 1000
 
 data_test_opt = {}
@@ -33,7 +33,7 @@ net_optionsF = {'userelu': False, 'depth': 28, 'widen_factor' : 10}
 pretrainedF = './experiments/miniImageNet_WRNLikeCosineClassifier/feat_model_net_epoch*.best'
 networks['feat_model'] = {'def_file': 'architectures/WideResNet.py', 'pretrained': pretrainedF, 'opt': net_optionsF, 'optim_params': None}
 
-net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.01, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(10*2, 0.1),(20*2, 0.006),(25*2, 0.0012),(30*2, 0.00024)]}
+net_optim_paramsC = {'optim_type': 'sgd', 'lr': 0.01, 'momentum':0.9, 'weight_decay': 5e-4, 'nesterov': True, 'LUT_lr':[(10*2, 0.01),(20*2, 0.006),(25*2, 0.0012),(30*2, 0.00024)]}
 pretrainedC = './experiments/miniImageNet_WRNLikeCosineClassifier/classifier_net_epoch*.best'
 net_optionsC = {'classifier_type': 'cosine', 'weight_generator_type': 'attention_based', 'nKall': nKbase, 'nFeat': 640, 'scale_cls': 10, 'scale_att': 10.0}
 networks['classifier'] = {'def_file': 'architectures/ClassifierWithFewShotGenerationModule.py', 'pretrained': pretrainedC, 'opt': net_optionsC, 'optim_params': net_optim_paramsC}
